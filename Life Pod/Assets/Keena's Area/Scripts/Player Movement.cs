26b305387d1eaf3032328940573d11170ebaaab4
ShaggySan <161761457+ShaggySan@users.noreply.github.com>
2024-04-30 17:02:33 -0400

    planet mag

---
+++ Life Pod/Assets/Keena's Area/Scripts/Player Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float playerSpeed = 25;
+    private Vector3 planetDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * playerSpeed * Time.deltaTime);
+        }
+    }
+}
